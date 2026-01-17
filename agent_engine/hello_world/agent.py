cbc1bfe3aaeccc567d8350d0b1163c7b91ac8940
Joonkyo Kim <rootwarp@gmail.com>
2026-01-18 00:23:15 +0900

    chore(agent_engine): deploy and update script

--- agent_engine/hello_world/agent.py
+++ agent_engine/hello_world/agent.py
@@ -4,7 +4,6 @@ from google.adk.tools import google_search
 
 root_agent = Agent(
     name="hello_world",
-    #model="gemini-3-pro-preview",
     model="gemini-2.5-flash",
     description="A friendly chatbot that can search the web for information.",
     instruction=(
